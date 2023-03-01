416bbc08cccb7db1ff798753fa2a67f3bf23f9bd
Ram81 <ramramrakhya81@gmail.com>
2023-03-01 10:32:26 -0500

    Add attr onav dataset

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ except:
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
-    for split, size in [("train", 10_000)]:
+    for split, size in [("train", 1000)]:
         with gzip.open(f"{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = NoCacheLazyJsonDataset(
-            data=houses, dataset="procthor-10k-uniform", split=split
+            data=houses, dataset="attr-onav-1k", split=split
         )
     return prior.DatasetDict(**data)
